47192ff73ba876ccff63935a9df1383893b06cbe
lawgamble <lsgrecording@gmail.com>
2021-08-29 13:14:55 -0500

    implemented error handlers

---
+++ back-end/src/errors/methodNotAllowed.js
@@ -0,0 +1,10 @@
+// Will check if specified method can be used for specific route.
+
+function methodNotAllowed(req, res, next) {
+  next({
+    status: 405,
+    message: `${req.method} not allowed for ${req.originalUrl}`,
+  });
+}
+
+module.exports = methodNotAllowed;
