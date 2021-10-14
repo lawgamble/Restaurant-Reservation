614a4cde43c131717fbed126e9317b24f25fd80b
lawgamble <lsgrecording@gmail.com>
2021-10-14 10:11:05 -0500

    Changed env port

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,4 +1,4 @@
-const { PORT = 5000 } = process.env;
+const { PORT } = process.env;
 
 const app = require("./app");
 const knex = require("./db/connection");
@@ -7,7 +7,7 @@ knex.migrate
   .latest()
   .then((migrations) => {
     console.log("migrations", migrations);
-    app.listen(PORT, listener);
+    app.listen(process.env.PORT || 5000, listener);
   })
   .catch((error) => {
     console.error(error);
