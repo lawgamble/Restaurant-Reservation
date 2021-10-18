dcc6b2e85b7a1ffcab253ec6954c2da06330340e
lawgamble <lsgrecording@gmail.com>
2021-10-18 14:10:09 -0500

    Header response

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -12,9 +12,18 @@ const tablesRouter = require("./tables/tables.router");
 
 const app = express();
 
-// app.use(cors());
-app.options("*", cors());
+app.use(cors());
+
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin. X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.use("/reservations", reservationsRouter);
 app.use("/tables", tablesRouter);
